d49f146607a4d0bd40b76820bf9fdfdacab79076
Jinjing Wu <wujinjing25@gmail.com>
2023-09-21 15:10:24 +0200

    feat: 90s grid mask and clock (#13)

---
+++ 03-the-fundamentals/90s/scripts/index.js
@@ -0,0 +1,20 @@
+const CLOCK_HOUR = document.querySelector(".hour");
+const CLOCK_COLON = document.querySelector(".colon");
+const CLOCK_MINUTE = document.querySelector(".minute");
+const CLOCK_MIDNIGHT = document.querySelector(".midnight");
+
+const tock = () => {
+  const now = new Date();
+  const hours = now.getHours() % 12;
+  const hour = hours ? hours : 12;
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const midnight = now.getHours() >= 12 ? 'pm' : 'am';
+
+  CLOCK_HOUR.textContent = hour;
+  CLOCK_MINUTE.textContent = minutes;
+  CLOCK_MIDNIGHT.textContent = midnight;
+}
+
+(() => {
+  setInterval(tock, 1000);
+})();
