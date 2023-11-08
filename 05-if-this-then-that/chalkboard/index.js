e76e4ca0ea1fc683e7b0e8e5ec1186623188fc2c
Jinjing Wu <jinjing.wu@compass.com>
2023-11-08 09:08:34 +0100

    small changes

--- 05-if-this-then-that/chalkboard/index.js
+++ 05-if-this-then-that/chalkboard/index.js
@@ -1,28 +1,19 @@
+"use strict";
 
+const writeButton = document.querySelector('#writeButton');
+const chalkboard = document.querySelector('#chalkboard');
 
-(()=> {
-  const writeButton = document.querySelector('#writeButton');
-  const chalkboard = document.querySelector('#chalkboard');
+const write = () => {
+  const el = document.createElement('li');
+  el.classList.add('chalkboard--item')
+  el.innerText="I WILL DEFINITELY USE CHATGPT!";
+  chalkboard?.append(el);
+}
 
-  const write = (container) => () => {
-    const el = document.createElement('li');
-    el.classList.add('chalkboard--item')
-    el.innerText="I WILL DEFINITELY USE CHATGPT!";
-    container?.append(el);
-  }
+writeButton.addEventListener('click', write);
 
-  writeButton.addEventListener('click', write(chalkboard));
-
-  const start = (counter) => {
-    if(counter < 1000){
-      setTimeout(() => {
-        counter++;
-        console.log(counter);
-        writeButton.click();
-        start(counter);
-      }, 500);
-    }
-  }
-
-  start(0);
-})();
+document.addEventListener('keydown', function(e) {
+  console.log({code: e.code})
+  if (e.code !== 'Space') return false;
+  write();
+});
