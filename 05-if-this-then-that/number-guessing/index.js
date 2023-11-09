f2b63a06609ec8be63a899f6578fae9417c70d5b
Jinjing Wu <jinjing.wu@compass.com>
2023-11-09 19:34:28 +0100

    feat: number guessing game with UI

--- 05-if-this-then-that/number-guessing/index.js
+++ 05-if-this-then-that/number-guessing/index.js
@@ -1,53 +1,117 @@
-'use strict'
-
-const context = {
-  lower: 0,       // lower limit for generating the guessing number `x`
-  upper: 100,     // upper limit for generating the guessing number `x`
-  x: null,        // the guessing number itself
-  count: null,    // how many counts required
-}
-
-const setBoundary = () => {
-  context.lower = Number(prompt("Enter the Lower bound"));
-  context.upper = Number(prompt("Enter the Upper bound"));
-}
-
-const generateRandomX = (lower, upper) => {
-  context.x = Math.floor(Math.random() * (upper - lower + 1) + lower);
-}
-
-const generateRequiredGuesses = (lower, upper) => {
-  context.count = Math.round(Math.log(upper - lower + 1));
-}
-
-const guessing = () => {
-  const {count, x} = context;
-  let tries = 0;
-  while (tries < count) {
-    tries += 1;
-    const guess = Number(prompt("Guess a number"))
-    if (guess === x) {
-      alert(`Congratulations you did it in ${tries} try`);
-      break;
-    } else if (guess > x) {
-      alert("You guessed too big!");
-    } else if (guess < x) {
-      alert("You guessed too small!");
-    } else {
-      alert("What have you done!!!!!!");
-    }
-  }
-
-  if (tries >= count) {
-    alert(`You have had your ${tries} tries!\n\nThe number is ${context.x}\n\nBetter Luck Next time!`);
-  }
-}
+'use strict';
 
 (() => {
-  setBoundary();
-  const {lower, upper} = context;
-  generateRandomX(lower, upper);
-  generateRequiredGuesses(lower, upper);
-  console.log(context);
-  guessing();
+  const context = {
+    lower: 0,       // lower limit for generating the guessing number `x`
+    upper: 100,     // upper limit for generating the guessing number `x`
+    x: undefined,        // the guessing number itself
+    count: undefined,    // how many counts maximum
+    left: undefined,     // how many counts left
+  }
+
+  const setBoundary = (lower, upper) => {
+    context.lower = lower;
+    context.upper = upper;
+  }
+
+  const generateRandomX = (lower, upper) => {
+    context.x = Math.floor(Math.random() * (upper - lower + 1) + lower);
+  }
+
+  const generateMaxGuesses = (lower, upper) => {
+    context.count = Math.round(Math.log(upper - lower + 1));
+  }
+
+  const setLeftGuesses = (times = context.count) => {
+    context.left = times;
+  }
+
+  const renderMaxTimes = (times) => {
+    const maxEle = document.querySelector('#guess__max-times-number');
+    maxEle.innerText = times;
+  }
+
+  const renderLeftTimes = (times = context.count) => {
+    const leftEle = document.querySelector('#guess__left-times-number');
+    leftEle.innerText = times;
+  }
+
+  const form = document.querySelector('#boundary--form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const boundary = Object.fromEntries(new FormData(e.target));
+    const lower = Number(boundary.lower);
+    const upper = Number(boundary.upper);
+
+    setBoundary(lower, upper);
+    generateRandomX(lower, upper);
+    generateMaxGuesses(lower, upper);
+    setLeftGuesses(context.count);
+
+    renderMaxTimes(context.count);
+    renderLeftTimes(context.count);
+
+    console.log(context);
+  });
+
+  const renderGuessResult = (result, modifierClass) => {
+    const resultEle = document.querySelector('#guess__result');
+    const animationClass = 'animate__rubberBand';
+    const showClass = 'guess__result_show';
+    const hideClass = 'guess__result_hide';
+
+    resultEle.innerText = result;
+    resultEle.classList.remove(hideClass);
+    resultEle.classList.add(animationClass, showClass);
+    modifierClass && resultEle.classList.add(modifierClass);
+
+    resultEle.addEventListener('animationend', (e) => {
+      resultEle.classList.add(hideClass);
+      e.target.classList.remove(animationClass, showClass);
+      modifierClass && resultEle.classList.remove(modifierClass);
+    }, {once: true})
+  }
+
+  const guessBtn = document.querySelector('#guess__number-btn');
+  const guessNumber = document.querySelector('#guess__number');
+
+  guessBtn.addEventListener('click', (e) => {
+    const {x, left = 0} = context;
+    const guessed = Number(guessNumber.value);
+
+    if (left === 0) {
+      renderGuessResult('What are you trying to do? OUT!', 'guess__result_success');
+      return;
+    }
+
+    context.left -= 1;
+    renderLeftTimes(context.left);
+
+    if (guessNumber.value === "") {
+      renderGuessResult(
+        'Guess due, why do you feed me with nothing, Haya!!!',
+        'guess__result_angry'
+      );
+      return;
+    }
+
+    if (x === guessed) {
+      renderGuessResult(`Congratulations, You did it!`, 'guess__result_success');
+    } else if (guessed > x) {
+      renderGuessResult("NO! TOO BIG!", 'guess__result_failed');
+    } else if (guessed < x) {
+      renderGuessResult("no. too small.", 'guess__result_failed');
+    } else {
+      renderGuessResult("WHAT HAVE YOU DONE!!!!!!", 'guess__result_angry');
+    }
+  });
+
+  const toggle = document.querySelector('#boundary__toggle');
+  toggle.addEventListener('click', () => {
+    const boundary = document.querySelector('#boundary');
+    boundary.classList.toggle('hide');
+  });
+
 })();
