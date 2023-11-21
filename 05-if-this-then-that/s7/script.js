3d0c6c1b1a79b146a1c03ebbc21d44dfad3a0090
Jinjing Wu <jinjing.wu@compass.com>
2023-11-21 21:15:04 +0100

    fix: messageEl needs to refer to div#message

--- 05-if-this-then-that/s7/script.js
+++ 05-if-this-then-that/s7/script.js
@@ -3,7 +3,7 @@
 let formEl = document.querySelector("#search");
 let cityInputEl = document.querySelector("#city");
 let tempEl = document.querySelector("#temp");
-let messageEl = document.querySelector("message");
+let messageEl = document.querySelector("#message");
 
 async function getData() {
   // Fetch data from Open Weather Map API, passing the input value as city
