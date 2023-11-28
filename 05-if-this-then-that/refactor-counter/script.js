15a7a52b5001151d3ce2f5364932fa094e1533e5
Jinjing Wu <jinjing.wu@compass.com>
2023-11-28 11:19:31 +0100

    extract the actions into func

--- 05-if-this-then-that/refactor-counter/script.js
+++ 05-if-this-then-that/refactor-counter/script.js
@@ -4,23 +4,38 @@ let counterValue = 0;
 const renderCounterValue = (counterValue) => {
   counter.innerHTML = counterValue;
 };
+
 renderCounterValue(counterValue);
 
-document.querySelector("#reset").addEventListener("click", function () {
-  counterValue = 0;
+const handleAction = (action) => {
+  switch (action) {
+    case "reset":
+      counterValue = 0;
+      break;
+    case "increment":
+      if (counterValue < 10) {
+        counterValue = counterValue + 1;
+      }
+      break;
+    case "decrement":
+      if (counterValue > 0) {
+        counterValue = counterValue - 1;
+      }
+      break;
+    default:
+      break;
+  }
   renderCounterValue(counterValue);
+};
+
+document.querySelector("#reset").addEventListener("click", function () {
+  handleAction("reset");
 });
 
 document.getElementById("increment").addEventListener("click", function () {
-  if (counterValue < 10) {
-    counterValue = counterValue + 1;
-    renderCounterValue(counterValue);
-  }
+  handleAction("increment");
 });
 
 document.getElementById("decrement").addEventListener("click", function () {
-  if (counterValue > 0) {
-    counterValue = counterValue - 1;
-    renderCounterValue(counterValue);
-  }
+  handleAction("decrement");
 });
