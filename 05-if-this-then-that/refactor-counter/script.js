3cf5fe66462cba1bc7688f86a2d4e4e9c3c75106
Jinjing Wu <jinjing.wu@compass.com>
2023-11-28 11:13:14 +0100

    extract the render part into a func

--- 05-if-this-then-that/refactor-counter/script.js
+++ 05-if-this-then-that/refactor-counter/script.js
@@ -1,23 +1,26 @@
 const counter = document.querySelector("#counter");
 let counterValue = 0;
 
-counter.innerHTML = counterValue;
+const renderCounterValue = (counterValue) => {
+  counter.innerHTML = counterValue;
+};
+renderCounterValue(counterValue);
 
 document.querySelector("#reset").addEventListener("click", function () {
   counterValue = 0;
-  counter.innerHTML = counterValue;
+  renderCounterValue(counterValue);
 });
 
 document.getElementById("increment").addEventListener("click", function () {
   if (counterValue < 10) {
     counterValue = counterValue + 1;
-    counter.innerHTML = counterValue;
+    renderCounterValue(counterValue);
   }
 });
 
 document.getElementById("decrement").addEventListener("click", function () {
   if (counterValue > 0) {
     counterValue = counterValue - 1;
-    counter.innerHTML = counterValue;
+    renderCounterValue(counterValue);
   }
 });
