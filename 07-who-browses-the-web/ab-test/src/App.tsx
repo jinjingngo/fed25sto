6430862061ede6b80e75de546d6abd9e6432877f
Jinjing Wu <jinjing.wu@compass.com>
2024-02-19 20:20:03 +0100

    feat: no react need for this experiment

--- 07-who-browses-the-web/ab-test/src/App.tsx
+++ 07-who-browses-the-web/ab-test/src/App.tsx
@@ -2,16 +2,10 @@ import "./App.css";
 import Static from "./components/Static/";
 import SilverBullet from "./components/SilverBullet";
 import NewsLetter from "./components/NewsLetter";
-import { useEffect, useState } from "react";
+
+const isSilverBulletEnabled = Math.random() >= 0.5;
 
 function App() {
-  const [isSilverBulletEnabled, setIsSilverBulletEnabled] = useState(false);
-
-  useEffect(() => {
-    const isSilverBulletEnabled = Math.random() >= 0.5;
-    setIsSilverBulletEnabled(isSilverBulletEnabled);
-  }, []);
-
   return (
     <>
       <Static />
